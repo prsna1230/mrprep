7984ad01b158572d03bd7790470312f77735a772
prsna1230 <ajithkumre212gmail.com>
2024-11-30 11:46:10 +0530

    feat(mrprep): create a server mrprep-3001

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log(server.js);
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+const PORT = process.env.PORT | 4321;
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
